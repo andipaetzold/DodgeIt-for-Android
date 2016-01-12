527226bc213a2dca7d317d09239fd14b225392ae
Andreas Pätzold <andi.paetzold@gmail.com>
2016-01-12 16:31:45 +0100

    Implemented about activity

--- app/src/main/java/de/andipaetzold/dodgeit/activities/AboutActivity.java
+++ app/src/main/java/de/andipaetzold/dodgeit/activities/AboutActivity.java
@@ -1,15 +1,29 @@
 package de.andipaetzold.dodgeit.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import de.andipaetzold.dodgeit.R;
 
-public class AboutActivity extends Activity {
+public class AboutActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        findViewById(R.id.about_button_github).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.about_button_github:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/andipaetzold/DodgeIt-for-Android")));
+                break;
+        }
     }
 }
